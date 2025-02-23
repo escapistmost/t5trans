7ae872dbc097fad495535ecc7c00e5e5ed87fec4
escapistmost <112916507+escapistmost@users.noreply.github.com>
2025-02-23 16:24:18 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,10 +1,9 @@
 import t5trans
 
-print(t5trans.trans_en2ch('hello world'))
-print(t5trans.trans_ru2ch('Здравствуйте, мир'))
-print(t5trans.trans_ch2ru('你好，世界'))
-print(t5trans.trans_ch2en('你好，世界'))
-print(t5trans.trans_en2ru('hello world'))
-print(t5trans.trans_ru2en('Здравствуйте, мир'))
-
+print("hello world 翻译为中文",t5trans.trans_en2ch('hello world'))
+print("Здравствуйте, мир 翻译为中文",t5trans.trans_ru2ch('Здравствуйте, мир'))
+print("你好，世界 翻译为俄文",t5trans.trans_ch2ru('你好，世界'))
+print("你好，世界 翻译为英文",t5trans.trans_ch2en('你好，世界'))
+print("hello world 翻译为俄文",t5trans.trans_en2ru('hello world'))
+print("Здравствуйте, мир 翻译为英文",t5trans.trans_ru2en('Здравствуйте, мир'))
 
